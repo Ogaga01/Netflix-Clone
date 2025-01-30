8bca4299573b55b9625609a64784be12a73b05fe
Ogaga <iyaraogaga69@gmail.com>
2025-01-30 16:59:19 +0100

    Add Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
 "use client";
-import { signOut, useSession } from "next-auth/react";
+import { useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
-import { useCurrentUser } from "@/hooks/useCurrentUser";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
-  const { data: user } = useCurrentUser();
   useSession({
     required: true,
     onUnauthenticated() {
@@ -14,16 +13,7 @@ export default function Home() {
 
   return (
     <>
-      <h1 className="text-4xl text-green-500">Hello there</h1>;
-      <p className="text-white">Logged in as {user?.name}</p>
-      <button
-        className="h-10 w-full bg-white"
-        onClick={() => {
-          signOut();
-        }}
-      >
-        Sign Out
-      </button>
+      <Navbar />
     </>
   );
 }
